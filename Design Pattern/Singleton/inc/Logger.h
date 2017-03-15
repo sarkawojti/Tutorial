2cc8dc91d0f415a5679115f0d5dbc070f1689aa9
wojciech_sarka <00wojti@gmail.com>
2017-03-15 17:08:38 +0100

    Thread-safe Singleton and cout method

--- Design Pattern/Singleton/inc/Logger.h
+++ Design Pattern/Singleton/inc/Logger.h
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <mutex>
 
-#pragma once;
+#pragma once
 
 class Logger
 {
@@ -11,19 +12,35 @@ public:
     static Logger& Instance();
 
     // inne metody i pola u¿ytkowe
-    void consolLog(const std::string&) const;
+    void consolLog(const std::string&);
 private:
     // private constructor
     Logger() = default;
+
+    //mutex odpowiada za synchronizacje miêdzy w¹tkami
+    //chroni wspó³dzielony obszar przed jednoczesnym dostêpem wielu w¹tków
+    static std::mutex createInstanceMutex;
+    std::mutex consolLogMutex;
 };
 
-void Logger::consolLog(const std::string& text) const
+std::mutex Logger::createInstanceMutex;
+
+void Logger::consolLog(const std::string& text)
 {
+    //thread-safe cout method
+    std::lock_guard<std::mutex> guard(consolLogMutex);
     std::cout << text << std::endl;
 }
 
 Logger& Logger::Instance()
 {
+   //thread-safe Singleton
+   //lock guard - koncepcja wzorca projektowego RAII
+   //zak³ada sekcjê krytyczn¹ (lock) i zwalnia j¹ w momencie gdy przestaje istnieæ (unlock).
+   //
+   //mo¿na równie¿ u¿yæ: unique_lock lub mutex.lock()/unlock()
+   //
+   std::lock_guard<std::mutex> guard(createInstanceMutex);
    static Logger instance;
    return instance;
 }
