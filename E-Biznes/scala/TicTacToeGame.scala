19f20704f2571b6ba286808e5e3837ff8de87ccb
wojciech_sarka <00wojti@gmail.com>
2017-03-26 16:25:59 +0200

    refactoring TicTacToeGame.scala

--- E-Biznes/scala/TicTacToeGame.scala
+++ E-Biznes/scala/TicTacToeGame.scala
@@ -5,51 +5,60 @@ class TicTacToe
     var board = Array(" ", " ", " ", " ", " ", " ", " ", " ", " ")
     var cross_move : Boolean = false
 
-    def play() =
+    def play() : Unit =
     {
-        var isNotEnd : Boolean = true
-        while(isNotEnd)
+        while(true)
         {
-            showBoard
-            println()
+            draw_a_board
             user_move
-            println()
-            if(isWin)
+            if(is_win)
             {
-                isNotEnd = false
+                draw_a_board
                 println("Koniec gry. Wygrales")
+                return
             }
-            if(isPat)
+            if(is_pat)
             {
-                isNotEnd = false
+                draw_a_board
                 println("Koniec gry. Remis")
+                return
             }
         }
     }
 
+  def get_move() : Int =
+  {
+      while(true)
+        {
+          if (cross_move)
+          {
+            print("Podaj gdzie wstawic krzyzyk [zakres 1-9]: ")
+          }
+          else
+          {
+            print("Podaj gdzie wstawic kolko [zakres 1-9]: ")
+          }
+          var index : Int = readInt() - 1
+
+          if ((8 < index) || (index < 0))
+            {
+              print("Podales zla wartosc [Z POZA ZAKRESU]. ")
+            }
+          else if (board(index) != " ")
+            {
+              print("Podales zla wartosc [POLE JEST JUZ ZAJETE].")
+            }
+          else
+            {
+              return index
+            }
+        }
+    return -1
+  }
   def user_move()
   {
-    var index : Int = 0
-    var not_correct_value : Boolean = true
-    while(not_correct_value)
-    {
-      if (cross_move) {
-        print("Podaj gdzie wstawic krzyzyk [zakres 1-9]: ")
-      }
-      else {
-        print("Podaj gdzie wstawic kolko [zakres 1-9]: ")
-      }
-      index = readInt()
-      index = index - 1
-      if ((8 < index) || (index < 0) || board(index) != " ")
-      {
-        print("Podales zla wartosc. ")
-      }
-      else
-      {
-        not_correct_value = false
-      }
-    }
+    var index : Int = get_move
+
     if(cross_move)
     {
       board(index) = "X"
@@ -62,9 +71,9 @@ class TicTacToe
     }
   }
 
-    def isPat() : Boolean =
+    def is_pat() : Boolean =
     {
-        for( i <- 0 to 8)
+        for( i <- 0 until board.size)
         {
             if(" " == board(i))
             {
@@ -74,7 +83,7 @@ class TicTacToe
         return true
     }
 
-    def isWin() : Boolean =
+    def is_win() : Boolean =
     {
         if(board(0) != " " && board(0) == board(1) && board(0) == board(2))
           return true;
@@ -92,16 +101,21 @@ class TicTacToe
           return true;
         if(board(2) != " " && board(2) == board(4) && board(2) == board(6))
           return true;
-        return true;
+        return false;
     }
 
-    def showBoard() =
+    def draw_a_board() =
     {
-        println(" " + board(0) + " | " + board(1) + " | " + board(2))
-        println("---+---+---")
-        println(" " + board(3) + " | " + board(4) + " | " + board(5))
-        println("---+---+---")
-        println(" " + board(6) + " | " + board(7) + " | " + board(8))
+        println()
+        for (index <- 0 until board.size by 3)
+          {
+            println(" " + board(index) + " | " + board(index+1) + " | " + board(index+2))
+            if(6 != index)
+            {
+              println("---+---+---")
+            }
+          }
+        println()
     }
 }
 
