328d8abbd174efd3b47eb8009bf49b2a775462e8
wojciech_sarka <00wojti@gmail.com>
2017-04-07 08:55:39 +0200

    Modify Command design

--- Design Pattern/Czynnosciowe/Polecenie/main.cpp
+++ Design Pattern/Czynnosciowe/Polecenie/main.cpp
@@ -1,23 +1,25 @@
 
 #include "inc/Invoker.h"
 #include "inc/Calculator.h"
-#include "inc/AddCommand.h"
-#include "inc/SubCommand.h"
+#include "inc/Command.h"
 
 int main()
 {
     Invoker<int> invoker;
     invoker.invoke(4, 6);
 
-    std::shared_ptr<ICommand<int>> add_command = std::make_shared<AddCommand<int>>();
+    Calculator<int> calculator;
+
+    using IntCommandType = Command<int, int(Calculator<int>::*)(const int&, const int&) const, Calculator<int>>;
+
+    std::shared_ptr<ICommand<int>> add_command = std::make_shared<IntCommandType>(calculator, &Calculator<int>::add);
+    std::shared_ptr<ICommand<int>> sub_command = std::make_shared<IntCommandType>(calculator, &Calculator<int>::sub);
 
     invoker.set_command(add_command);
     int param1 = 4, param2 = 7;
     std::cout << param1 << " + " << param2 << " = " << invoker.invoke(param1, param2) << std::endl;
     param1 = 4, param2 = -7;
     std::cout << param1 << " + " << param2 << " = " << invoker.invoke(param1, param2) << std::endl;
-
-    std::shared_ptr<ICommand<int>> sub_command = std::make_shared<SubCommand<int>>();
 
     invoker.set_command(sub_command);
     param1 = 4, param2 = 7;
